c78b565263854243aff2975e7ec828e84cd8410a
Ahtisham Javed <ahtiishamjaved223@gmail.com>
2024-03-02 01:22:55 +0500

    task1

--- task1.rs
+++ task1.rs
@@ -1,15 +1,21 @@
-fn concatenate_strings(string1: &str, string2: &str) -> String {
+fn concatenate_strings(s1: &str, s2: &str) -> String {
     let mut result = String::new();
-    result.push_str(string1);
-    result.push_str(string2);
+    
+    result.push_str(s1);
+    result.push_str(s2);
+    
+    // Return the result string
     result
 }
 
 fn main() {
+    // Initialize two String variables string1 and string2
     let string1 = String::from("Hello, ");
     let string2 = String::from("world!");
 
+    // Call the concatenate_strings function with references to string1 and string2
     let concatenated_string = concatenate_strings(&string1, &string2);
 
+    // Print the result to the console
     println!("{}", concatenated_string);
 }
